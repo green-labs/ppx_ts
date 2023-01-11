9ba536868768598d776b6167df8f088e3b99107a
Woonki Moon <woonki.moon@gmail.com>
2023-01-11 14:10:48 +0900

    add polyvari toArray

--- src/sig_to_array.ml
+++ src/sig_to_array.ml
@@ -5,25 +5,32 @@ open Utils
 
 (* keyOf attribute mapper *)
 let make_signature_items name loc manifest kind suffix =
+  let value_binding =
+    [
+      Sig.value ~loc
+        (Val.mk
+           (mkloc (name ^ "_" ^ suffix) loc)
+           (Typ.constr (Utils.lid "array")
+              [ Typ.constr (Utils.lid "string") [] ]));
+    ]
+  in
+
   match (manifest, kind) with
-  | None, Ptype_abstract -> fail loc "Can't handle the unspecified type"
+  | Some { ptyp_desc = Ptyp_variant (_, _, _) }, Ptype_abstract
   | None, Ptype_variant _ ->
-      [
-        Sig.value ~loc
-          (Val.mk
-             (mkloc (name ^ "_" ^ suffix) loc)
-             (Typ.constr (Utils.lid "array")
-                [ Typ.constr (Utils.lid "string") [] ]));
-      ]
+      value_binding
   | _ -> fail loc "This type is not handled by ppx_ts"
 
 (* keyOf extension mapper *)
 let make_new_signature_item name loc manifest kind attributes =
+  let value_description =
+    Sig.value ~loc
+      (Val.mk ~loc ~attrs:attributes (mkloc name loc)
+         (Typ.constr (Utils.lid "array") [ Typ.constr (Utils.lid "string") [] ]))
+  in
+
   match (manifest, kind) with
-  | None, Ptype_abstract -> fail loc "Can't handle the unspecified type"
+  | Some { ptyp_desc = Ptyp_variant (_, _, _) }, Ptype_abstract
   | None, Ptype_record _ ->
-      Sig.value ~loc
-        (Val.mk ~loc ~attrs:attributes (mkloc name loc)
-           (Typ.constr (Utils.lid "array")
-              [ Typ.constr (Utils.lid "string") [] ]))
+      value_description
   | _ -> fail loc "This type is not handled by ppx_ts"
